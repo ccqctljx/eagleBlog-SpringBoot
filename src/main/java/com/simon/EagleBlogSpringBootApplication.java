cc5bb796ce3c24ec056c905c56605ec36798fc53
Simon <ccqctljx@gmail.com>
2020-02-20 19:33:07 +0800

    init

---
+++ src/main/java/com/simon/EagleBlogSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.simon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EagleBlogSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EagleBlogSpringBootApplication.class, args);
+    }
+
+}
